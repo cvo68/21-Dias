40a813e16fb17f1336b900d42f2ef2024e0baa23
Charliston Vilela <cvo68@outlook.com>
2024-01-31 21:08:55 -0300

    Commit dia 3

---
+++ Dia 3/script.js
@@ -0,0 +1,32 @@
+let nome = "Charliston Vilela";
+let idade = 27;
+let verdade = true;
+
+console.log(nome);
+console.log(idade);
+console.log(verdade);
+
+nome = "Kekel";
+console.log(nome)
+
+console.log ("---------------------------");
+
+let n1 = 3;
+let n2 = 4;
+
+let soma = n1 + n2;
+let sub = n1 - n2;
+let mult = n1 * n2;
+let div = n1 / n2;
+
+console.log(soma);
+console.log(sub);
+console.log(mult);
+console.log(div);
+
+console.log ("---------------------------");
+
+let texto = "68";
+let num = Number(texto);
+
+console.log(num);
