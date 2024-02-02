e6db74922d5574b5db02176451d94bd2f0dcab51
Charliston Vilela <cvo68@outlook.com>
2024-02-02 13:56:27 -0300

    Commit do dia 4

---
+++ Dia 4/script.js
@@ -0,0 +1,11 @@
+let fome = true;
+let dinheiro = true;
+
+if (fome === false && dinheiro === false){
+    console.log("Hoje a janta sera em casa!!");
+} else if(fome === true || dinheiro === false){
+    console.log("Voce precisa comer de qualquer jeito!!");
+} else{
+    console.log("Voce tem o luxo de jantar fora hoje...")
+}
+
